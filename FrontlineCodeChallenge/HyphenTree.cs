7b239db086c559b9739ef67bad091c94b4453a15
asifnavaz <asif.navaz@gmail.com>
2016-07-23 15:45:05 -0500

    commiting first version

---
+++ FrontlineCodeChallenge/HyphenTree.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrontlineCodeChallenge
+{
+
+    /// <summary>
+    /// This class is used to parse the string provided to a tree structure by appending hyphens to inner node
+    /// </summary>
+    public class HyphenTree
+    {
+        public string strTitle { get; set; }
+        public List<HyphenTree> lstChild { get; set; }
+
+        /// <summary>
+        /// This method prints the result by accepting the tree level as param
+        /// </summary>
+        /// <param name="level"></param>
+        public void PrintTree(int level)
+        {
+            if (string.IsNullOrEmpty(strTitle))
+                return;
+
+            //Appends the hyphen based on the tree level passed as argument
+            for (int i = 0; i < level-1; i++)
+            {
+                Console.Write("-");
+            }
+
+            Console.WriteLine(strTitle);
+
+            //calling the print function recursive to build the tree
+            if (lstChild != null)
+            {
+                foreach (HyphenTree objHtree in lstChild)
+                    objHtree.PrintTree(level + 1);
+            }
+                
+
+        }
+
+        /// <summary>
+        /// This method sorts the results in the list by Title field
+        /// </summary>
+        public void Sort()
+        {
+            lstChild = lstChild.OrderBy(objHtree => objHtree.strTitle).ToList();
+            foreach (HyphenTree objHtree in lstChild)
+            {
+                objHtree.Sort();
+            }
+                
+        }
+    }
+}
---
+++ FrontlineCodeChallenge/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrontlineCodeChallenge
+{
+    /// <summary>
+    /// This entry point which utilizes the Hyphen class to manipulate the- 
+    /// string provided into a tree structure as requested by Frontline
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strInput = "id,created,employee(id,firstname,employeeType(id),lastname),location";
+            strInput += ",";
+
+            Stack<HyphenTree> stackItems = new Stack<HyphenTree>();
+            int intStart = 0;
+            int intPosition = 0;
+
+            HyphenTree objRoot = new HyphenTree { strTitle = "Result Tree", lstChild = new List<HyphenTree>() };
+            foreach (char chrInput in strInput)
+            {
+                intPosition++;
+                if (chrInput == '(')
+                {
+                    HyphenTree objChildRoot = new HyphenTree { strTitle = strInput.Substring(intStart, intPosition - intStart - 1), lstChild = new List<HyphenTree>() };
+                    objRoot.lstChild.Add(objChildRoot);
+                    stackItems.Push(objRoot);
+                    objRoot = objChildRoot;
+                    intStart = intPosition;
+                }
+
+                if (chrInput == ',')
+                {
+                    if (strInput[intPosition - 1] == ')')
+                    {
+                        intStart = intPosition;
+                        continue;
+                    }
+
+                    objRoot.lstChild.Add(new HyphenTree { strTitle = strInput.Substring(intStart, intPosition - intStart - 1), lstChild = new List<HyphenTree>() });
+                    intStart = intPosition;
+                }
+
+                if (chrInput == ')')
+                {
+                    objRoot.lstChild.Add(new HyphenTree { strTitle = strInput.Substring(intStart, intPosition - intStart - 1), lstChild = new List<HyphenTree>() });
+                    objRoot = stackItems.Pop();
+                    intStart = intPosition;
+                }
+
+            }
+
+            Console.WriteLine("Result before Sorting");
+            Console.WriteLine("---------------------");
+            objRoot.PrintTree(0);
+
+            //Bonus question was provided to sort the result in alphabetical order
+            Console.WriteLine("Result After Sorting");
+            Console.WriteLine("---------------------");
+            objRoot.Sort();
+            objRoot.PrintTree(0);
+                        
+        }
+    }
+}
